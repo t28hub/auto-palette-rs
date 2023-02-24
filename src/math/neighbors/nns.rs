85eedec35e5d9f3c8fca4429f4089981cfe59f30
t28hub <t28oogle@gmail.com>
2023-02-24 20:22:49 +0900

    Add unit tests for Neighbor

--- src/math/neighbors/nns.rs
+++ src/math/neighbors/nns.rs
@@ -1,6 +1,19 @@
 use crate::math::number::FloatNumber;
 use std::fmt::{Display, Formatter, Result};
 
+/// Trait to search for neighbors.
+pub(crate) trait NearestNeighborSearch<F, T>
+where
+    F: FloatNumber,
+{
+    /// Search k-nearest neighbor points.
+    fn search(&self, query: T, k: usize) -> Vec<Neighbor<F>>;
+
+    /// Search nearest neighbor point.
+    fn search_nearest(&self, query: T) -> Option<Neighbor<F>>;
+}
+
+/// A neighbor point.
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
 pub(crate) struct Neighbor<F: FloatNumber> {
     pub index: usize,
@@ -24,16 +37,25 @@ impl<F> Neighbor<F>
 where
     F: FloatNumber,
 {
+    /// Create a new neighbor point.
     pub fn new(index: usize, distance: F) -> Self {
         Self { index, distance }
     }
 }
 
-pub(crate) trait NearestNeighborSearch<F, T>
-where
-    F: FloatNumber,
-{
-    fn search(&self, query: T, k: usize) -> Vec<Neighbor<F>>;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    fn search_nearest(&self, query: T) -> Option<Neighbor<F>>;
+    #[test]
+    fn new_should_create_neighbor() {
+        let neighbor = Neighbor::new(3, 2.0.sqrt());
+        assert_eq!(
+            neighbor,
+            Neighbor {
+                index: 3,
+                distance: 2.0.sqrt()
+            }
+        );
+    }
 }
